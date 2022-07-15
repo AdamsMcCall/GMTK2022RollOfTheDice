9e6c49ed4b97d25f3d2726059b9c1e8433bd3d70
Adam Akkari <adam.akkari@epitech.eu>
2022-07-16 01:16:10 +0200

    Added limits to movement in DiceBehavior

--- Assets/DiceBehavior.cs
+++ Assets/DiceBehavior.cs
@@ -19,26 +19,29 @@ public class DiceBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && grid_x > 0)
         {
             transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
             grid_x -= 1;
+            grid.GetGridValue(grid_x, grid_y);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && grid_x < grid.Width - 1)
         {
             transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
             grid_x += 1;
+            grid.GetGridValue(grid_x, grid_y);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grid_y < grid.Height - 1)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
             grid_y += 1;
+            grid.GetGridValue(grid_x, grid_y);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && grid_y > 0)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
             grid_y -= 1;
+            grid.GetGridValue(grid_x, grid_y);
         }
-        //grid.GetGridValue(grid_x, grid_y);
     }
 }
