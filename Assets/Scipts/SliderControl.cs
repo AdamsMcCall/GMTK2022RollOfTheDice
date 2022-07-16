98d048d62c21421ae61b0cffa1cf1eb7875610e8
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2022-07-16 21:38:05 +0200

    Added mesh and started slider script

---
+++ Assets/Scipts/SliderControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{
+    private float timer = 1.5f;
+    public Slider slider;
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = 1.5f;
+            slider.value -= 10;
+        } 
+        
+    }
+}
