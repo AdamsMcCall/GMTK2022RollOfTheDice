2d50d458e4d7fded5bace03cc17ee9e13d12f629
Adam Akkari <adam.akkari@epitech.eu>
2022-07-17 00:44:40 +0200

    Added probability management for grid generation

--- Assets/Scripts/GridBehavior.cs
+++ Assets/Scripts/GridBehavior.cs
@@ -11,6 +11,8 @@ public class GridBehavior : MonoBehaviour
 
     public List<GameObject> TileTypes;
 
+    public List<int> TileProbability;
+
     //private List<ITile> TileGrid;
 
     private ITile[,] TileGrid;
@@ -34,10 +36,40 @@ public class GridBehavior : MonoBehaviour
         {
             for (int x = 0; x < Width; ++x)
             {
-                var obj = Instantiate(TileTypes[Random.Range(0, TileTypes.Count)], new Vector3(x, 0, y), Quaternion.identity, transform);
+                GameObject obj;
+
+                if (TileTypes.Count != TileProbability.Count)
+                {
+                    obj = Instantiate(TileTypes[Random.Range(0, TileTypes.Count)], new Vector3(x, 0, y), Quaternion.identity, transform);
+                }
+                else
+                {
+                    obj = Instantiate(GetTileTypeFromProbability(), new Vector3(x, 0, y), Quaternion.identity, transform);
+                }
+
                 TileGrid[x, y] = obj.GetComponent(typeof(ITile)) as ITile;
             }
         }
+    }
+
+    private GameObject GetTileTypeFromProbability()
+    {
+        var totalProbability = 0;
+        TileProbability.ForEach(x => totalProbability += x);
+        var randomNumber = Random.Range(0, totalProbability);
+        int i = 0;
+        int j = 0;
+
+        foreach (int probability in TileProbability)
+        {
+            if (randomNumber >= i && randomNumber < i + probability)
+            {
+                return TileTypes[j];
+            }
+            i += probability;
+            ++j;
+        }
+        return TileTypes[Random.Range(0, TileTypes.Count)];
     }
 
     // Update is called once per frame
