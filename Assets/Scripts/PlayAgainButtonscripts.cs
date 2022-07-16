bc49354242a4e92cb275ced269cc2b2cb586e1bd
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2022-07-17 01:08:58 +0200

    Added Replay and Quit Button

---
+++ Assets/Scripts/PlayAgainButtonscripts.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class PlayAgainButtonscripts : MonoBehaviour
+{
+    public Button replayButton;
+    public Button quitbutton;
+    
+
+    private void Start()
+    {
+        replayButton.onClick.AddListener(Replay);
+        quitbutton.onClick.AddListener(Quit);
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("SampleScene",LoadSceneMode.Single);
+    }
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
