2e3ad729d3f97e82e25ba190639febd72d9ecbf5
Adam Akkari <adam.akkari@epitech.eu>
2022-07-17 20:21:25 +0200

    Added increase in difficulty

--- Assets/Scripts/LevelManagement.cs
+++ Assets/Scripts/LevelManagement.cs
@@ -8,6 +8,7 @@ public class LevelManagement : MonoBehaviour
     public List<GameObject> Levels;
     public GameObject LevelPrefab;
     private Scorekeeper scoreKeeper;
+    private int levelNumber = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +31,8 @@ public class LevelManagement : MonoBehaviour
         nextDice.Initialize();
         currentDice.preventMove = true;
 
+        levelNumber += 1;
+
         StartCoroutine(TranslateLevelRoutine(currentLevel, false));
         StartCoroutine(TranslateLevelRoutine(nextLevel, true));
     }
@@ -43,6 +46,52 @@ public class LevelManagement : MonoBehaviour
         var grid = newLevel.GetComponentInChildren<GridBehavior>();
 
         scoreKeeper.goal += 40;
+        if (levelNumber > 0 && levelNumber <= 5)
+        {
+            grid.Width = 5;
+            grid.Height = 5;
+            grid.TileProbability[0] = 6;
+            grid.TileProbability[1] = 2;
+            grid.TileProbability[2] = 0;
+            grid.TileProbability[3] = 0;
+        }
+        else if (levelNumber > 5 && levelNumber <= 10)
+        {
+            grid.Width = 6;
+            grid.Height = 6;
+            grid.TileProbability[0] = 6;
+            grid.TileProbability[1] = 3;
+            grid.TileProbability[2] = 0;
+            grid.TileProbability[3] = 1;
+        }
+        else if (levelNumber > 10 && levelNumber <= 15)
+        {
+            grid.Width = 7;
+            grid.Height = 7;
+            grid.TileProbability[0] = 6;
+            grid.TileProbability[1] = 4;
+            grid.TileProbability[2] = 0;
+            grid.TileProbability[3] = 2;
+        }
+        else if (levelNumber > 15 && levelNumber <= 20)
+        {
+            grid.Width = 8;
+            grid.Height = 8;
+            grid.TileProbability[0] = 6;
+            grid.TileProbability[1] = 5;
+            grid.TileProbability[2] = 0;
+            grid.TileProbability[3] = 3;
+        }
+        else
+        {
+            grid.Width = 9;
+            grid.Height = 9;
+            grid.TileProbability[0] = 6;
+            grid.TileProbability[1] = 6;
+            grid.TileProbability[2] = 0;
+            grid.TileProbability[3] = 4;
+        }
+       
 
         grid.GameEnvironment = this.gameObject;
         grid.GenerateGrid();
