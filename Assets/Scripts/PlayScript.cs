31d5472408e5b5e4d8e88c2927faab58381e48da
MatthiasHelderald <109383974+MatthiasHelderald@users.noreply.github.com>
2022-07-17 12:14:32 +0200

    Added Start Game UI

---
+++ Assets/Scripts/PlayScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class PlayScript : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitbutton;
+    
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(play);
+        quitbutton.onClick.AddListener(Quit);
+    }
+
+    public void play()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
